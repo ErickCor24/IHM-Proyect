eea7cdae78fccb81abc6f631a935a3a624b3ea85
camillie15 <138990540+camillie15@users.noreply.github.com>
2024-07-27 14:18:45 -0500

    Inicio.css | Inicio.jsx

--- src/components/Main/Inicio.jsx
+++ src/components/Main/Inicio.jsx
@@ -2,13 +2,28 @@ import { Link } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom';
 import { RedirectSecretaria } from './RedirectSecretaria';
 import { RedirectDocente } from './RedirectDocente';
+import './Inicio.css';
 
 export function Inicio (){
     return(
         <>
-            <Link className='' to='/Docente'><button className='btn-options'>&#9658; Profesor</button></Link>
-            <Link className='' to='/Secretaria'><button className='btn-options'>&#9658; Secretaria</button></Link>
-            <h1>Inicio</h1>
+            <div className='container'>
+                <h1>Inicio Sesión</h1>
+                <div className='container-inputs'>
+                    <div className='inputs-data'>
+                        <h3>Usuario</h3>
+                        <input type='text'></input>
+                    </div>
+                    <div className='inputs-data'>
+                        <h3>Contraseña</h3>
+                        <input type='password'></input>
+                    </div>
+                </div>
+                <div className='container-buttons'>
+                    <Link className='buttons' to='/Docente'><button className='btn-options'>&#9658; Profesor</button></Link>
+                    <Link className='buttons' to='/Secretaria'><button className='btn-options'>&#9658; Secretaria</button></Link>
+                </div>
+            </div>
 
             <Routes>
                 <Route path='/Secretaria' element={<RedirectSecretaria />} />
