98a42aca1f243f99f7e14fca59939c4928fa1c12
ErickCor24 <erick.cordovavit@ug.edu.ec>
2024-08-04 00:23:59 -0500

    Creacion de tarjetas en home

--- src/components/Main/Home.jsx
+++ src/components/Main/Home.jsx
@@ -1,4 +1,5 @@
 import './Home.css'
+import { Link } from 'react-router-dom'
 export function HomeDocente () {
     return(
         <>
@@ -10,8 +11,31 @@ export function HomeDocente () {
 
 export function HomeSecretaria () {
     return(
-        <>
+        <section className='home-section'>
             <h1>Bienvenido Monica Mendoza</h1>
-        </>
+            
+            <div className='cards-section'>
+
+                <Link  to='./Matriculacion' className='link-card card card-secretaria'>
+                        <div className='card-img-bg one'/>
+                        <label className='label-home'> Matriculacion estudiante</label>
+                </Link>
+
+                <Link to='./Update' className='link-card card card-secretaria'>
+                        <div className='card-img-bg  two'/>
+                        <label className='label-home'>Cambiar curso estudiante</label>
+                </Link>
+
+                <Link to='./Delete' className='link-card card card-secretaria'>
+                    <div className='card-img-bg three'/>
+                    <label className='label-home'>Anular matricula</label>
+                </Link>
+
+                <Link to='./Vista' className='link-card card card-secretaria'>
+                    <div className='card-img-bg four'/>
+                    <label className='label-home'>Visualizar cupos y cursos</label>
+                </Link>                
+            </div>
+        </section>
     )
 }
