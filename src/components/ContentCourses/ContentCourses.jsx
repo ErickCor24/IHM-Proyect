c2b322c07e819053ddb7162d55c5e47c499572ad
camillie15 <138990540+camillie15@users.noreply.github.com>
2024-08-03 19:19:18 -0500

    ContentCourses.jsx y ContentCourses.css

--- src/components/ContentCourses/ContentCourses.jsx
+++ src/components/ContentCourses/ContentCourses.jsx
@@ -1,10 +1,48 @@
-
+import './ContentCourses.css';
 export function ContentCourses ({course}) {
     return (
         <>
-            <h3>{course}</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum impedit similique eaque ipsa laboriosam reprehenderit eveniet ratione vel eos soluta? Optio iste corporis suscipit dolorum alias ducimus sapiente ipsa ut.</p>
-
+            <div className='content'>
+                <h1>{course}</h1>
+                <h2>Estudiantes</h2>
+                <div className='content-table'>
+                    <table className='table-notas'>
+                        <tr className='table-head'>
+                            <th>Estudiante</th>
+                            <th>G.Formativa</th>
+                            <th>G.Práctica</th>
+                            <th>Acre. y Val.</th>
+                        </tr>
+                        <tr className='table-row'>
+                            <td>Daniel Mateo Aguilar Villafuerte</td>
+                            <td>9</td>
+                            <td>9.5</td>
+                            <td>10</td>
+                        </tr>
+                        <tr className='table-row'>
+                            <td>Camillie Thais Ayovi Villafuerte</td>
+                            <td>10</td>
+                            <td>9.5</td>
+                            <td>10</td>
+                        </tr>
+                        <tr className='table-row'>
+                            <td>Erick Alejandro Cordova Viteri</td>
+                            <td>10</td>
+                            <td>10</td>
+                            <td>9.5</td>
+                        </tr>
+                        <tr className='table-row'>
+                            <td>Cesar Alexander Tipan Anton</td>
+                            <td>10</td>
+                            <td>9.5</td>
+                            <td>9.9</td>
+                        </tr>
+                    </table>
+                </div>
+                <div className='content-button'>
+                        <button className='button-con'> Registrar</button>
+                    </div>
+            </div>
         </>
     )
 }
