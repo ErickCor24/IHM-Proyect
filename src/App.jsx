5c1d721c2455291e6ecd8db821b0cef0b5fb66a9
Cesar Tipan <cesar.tipanant@ug.edu.ec>
2024-08-05 21:57:15 -0500

    TEST

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RedirectSecretaria } from './components/Main/RedirectSecretaria.jsx';
 import { Matriculacion } from './components/Content/Matriculacion.jsx';
 import { Inicio } from './components/Main/Inicio.jsx';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RedirectDocente } from './components/Main/RedirectDocente.jsx';
 import { RegistrarNota } from './components/ContentCourses/RegistrarNota.jsx';
 
-export function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/Secretaria/*" element={<RedirectSecretaria />} />
-        <Route path="/Docente/*" element={<RedirectDocente />} />
-      </Routes>
-    </BrowserRouter>
-  );
+export function App(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Inicio />} /> 
+                <Route path="/Secretaria/*" element={<RedirectSecretaria />} />
+                <Route path="/Docente/*" element={<RedirectDocente />} />
+                {/* <Route path={`/Docente/:course./Registrar`} element={<RegistrarNota />} /> */}
+                </Routes>
+        </BrowserRouter>
+    );
 }
 
-export function handleDoNothing(event) {
-  event.preventDefault();
-}
+export function handleDoNothing(event){
+    event.preventDefault();
+};
